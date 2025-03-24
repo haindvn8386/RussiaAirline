d2066331858d9845853220577634e8bbc5ec9dee
Administrator <haindvn@gmail.com>
2025-03-24 22:09:58 +0700

    danh sách các bảng trong database Airline bao gồm:
    aircrafts_data
    airports_data
    boarding_passes
    bookings
    flights
    seats
    ticket_flights
    tickets

---
+++ src/main/java/rediscache/russiaairline/RussiaAirlineApplication.java
@@ -0,0 +1,13 @@
+package rediscache.russiaairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RussiaAirlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RussiaAirlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rediscache/russiaairline/RussiaAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package rediscache.russiaairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RussiaAirlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
